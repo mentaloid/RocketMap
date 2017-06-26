f23018e24e166069567768ec529fe0811b9e042e
FrostTheFox <thunderfox@thunderfox.nl>
2017-06-26 16:42:19 -0400

    another map.js fix

--- static/js/map.js
+++ static/js/map.js
@@ -610,7 +610,7 @@ str += `
                     <div>
                         Last Modified: ${lastModifiedStr}
                     </div>
-                    ${directionStr}
+                    ${directionsStr}
                 </center>
             </div>`
 
