a909e39519b5bc6587bea0ac499c548167ea5923
FrostTheFox <thunderfox@thunderfox.nl>
2017-06-26 16:26:13 -0400

    models.py missed stuff

--- pogom/models.py
+++ pogom/models.py
@@ -2499,26 +2499,26 @@ def parse_gyms(args, gym_responses, wh_update_queue, db_update_queue):
 
             if args.webhooks:
                 webhook_data['pokemon'].append({
-                    'pokemon_uid': member['pokemon_data']['id'],
-                    'pokemon_id': member['pokemon_data']['pokemon_id'],
-                    'cp': member['pokemon_data']['cp'],
-                    'num_upgrades': member['pokemon_data'].get(
+                    'pokemon_uid': pokemon['id'],
+                    'pokemon_id': pokemon['pokemon_id'],
+                    'cp': pokemon['cp'],
+                    'num_upgrades': pokemon.get(
                         'num_upgrades', 0),
-                    'move_1': member['pokemon_data'].get('move_1'),
-                    'move_2': member['pokemon_data'].get('move_2'),
-                    'height': member['pokemon_data'].get('height_m'),
-                    'weight': member['pokemon_data'].get('weight_kg'),
-                    'stamina': member['pokemon_data'].get('stamina'),
-                    'stamina_max': member['pokemon_data'].get('stamina_max'),
-                    'cp_multiplier': member['pokemon_data'].get(
+                    'move_1': pokemon.get('move_1'),
+                    'move_2': pokemon.get('move_2'),
+                    'height': pokemon.get('height_m'),
+                    'weight': pokemon.get('weight_kg'),
+                    'stamina': pokemon.get('stamina'),
+                    'stamina_max': pokemon.get('stamina_max'),
+                    'cp_multiplier': pokemon.get(
                         'cp_multiplier'),
-                    'additional_cp_multiplier': member['pokemon_data'].get(
+                    'additional_cp_multiplier': pokemon.get(
                         'additional_cp_multiplier', 0),
-                    'iv_defense': member['pokemon_data'].get(
+                    'iv_defense': pokemon.get(
                         'individual_defense', 0),
-                    'iv_stamina': member['pokemon_data'].get(
+                    'iv_stamina': pokemon.get(
                         'individual_stamina', 0),
-                    'iv_attack': member['pokemon_data'].get(
+                    'iv_attack': pokemon.get(
                         'individual_attack', 0),
                     'trainer_name': member['trainer_public_profile']['name'],
                     'trainer_level': member['trainer_public_profile']['level'],
